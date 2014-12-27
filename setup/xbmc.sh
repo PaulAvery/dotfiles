ed986e793040e6bcc5e4ca6324b6ed0a862743b7
Florian Albertz <flo.albertz@gmail.com>
2014-12-27 15:38:18 +0100

    Put xrandr lines into .xinitrc

--- setup/xbmc.sh
+++ setup/xbmc.sh
@@ -6,14 +6,18 @@ pacman -S --noconfirm xbmc shairport libplist lirc udisks unrar
 
 # Setup autostart
 aura -A --noconfirm xbmc-standalone-service
+systemctl enable xbmc
+
 touch /etc/X11/Xwrapper.config
 echo "allowed_users = anybody" >> /etc/X11/Xwrapper.config
 echo "needs_root_rights = yes" >> /etc/X11/Xwrapper.config
 
-# Fix weathered colors
+# Fix weathered colors (Still need to add /home/xbmc/.xinitrc to your /usr/bin/xbmc-standalone2 script by hand)
 pacman -S --noconfirm xorg-xrandr
-touch /etc/X11/xinit/xinitrc.d/10-fix-colors
-chmod +x /etc/X11/xinit/xinitrc.d/10-fix-colors
-echo "xrandr --output HDMI1 --set \"Broadcast RGB\" \"Full\"" >> /etc/X11/xinit/xinitrc.d/10-fix-colors
-echo "xrandr --output HDMI2 --set \"Broadcast RGB\" \"Full\"" >> /etc/X11/xinit/xinitrc.d/10-fix-colors
-echo "xrandr --output HDMI3 --set \"Broadcast RGB\" \"Full\"" >> /etc/X11/xinit/xinitrc.d/10-fix-colors
+mkdir /home/xbmc
+touch /home/xbmc/.xinitrc
+chmod +x /home/xbmc/.xinitrc
+echo "#!/bin/bash" >> /home/xbmc/.xinitrc
+echo "xrandr --output HDMI1 --set \"Broadcast RGB\" \"Full\"" >> /home/xbmc/.xinitrc
+echo "xrandr --output HDMI2 --set \"Broadcast RGB\" \"Full\"" >> /home/xbmc/.xinitrc
+echo "xrandr --output HDMI3 --set \"Broadcast RGB\" \"Full\"" >> /home/xbmc/.xinitrc
