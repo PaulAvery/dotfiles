5008f8bd6c4b9290f3475625c479ed08e81b1bb2
Florian Albertz <flo.albertz@gmail.com>
2015-02-25 18:40:40 +0100

    Remove setup from dotfiles

--- setup/base/zsh.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Install zsh and set as default
-pacman --noconfirm -S zsh grml-zsh-config zsh-syntax-highlighting
-
-chsh -s $(which zsh) $USER
--- setup/desktop.sh
+++
@@ -1,9 +0,0 @@
-# Copy dotfiles
-ln -s $(pwd)/../{.wpmanager,.Xresources} ~
-ln -s $(pwd)../{bar,herbstluftwm,user-dirs.dirs} ~/.config
-
-# Install bar script dependencies
-cd ~/.config/bar && npm install
-
-# Install wallpaper manager
-sudo npm install -g wpmanager
