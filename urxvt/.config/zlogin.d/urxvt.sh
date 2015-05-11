62097450848a703844267f123350f1647ff6c0ab
Florian Albertz <flo.albertz@gmail.com>
2015-05-11 23:25:31 +0200

    Add own urxvt package

---
+++ urxvt/.config/zlogin.d/urxvt.sh
@@ -0,0 +1,8 @@
+# Merge .Xresources
+xrdb -merge .Xresources
+
+# Set urxvt colors
+TEMPLATE="{{#.}}URxvt*color{{@key}}: #{{hex.[0]}}{{hex.[1]}}{{hex.[2]}}
+{{/.}}"
+
+tstore template "color-themes.$(tstore template color-themes.selected {{.}})" "$TEMPLATE" | xrdb -merge
