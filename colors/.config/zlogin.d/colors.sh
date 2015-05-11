beb570e49977750b6174d36392744e63e36bf069
Florian Albertz <flo.albertz@gmail.com>
2015-05-11 23:26:00 +0200

    Add colors package

---
+++ colors/.config/zlogin.d/colors.sh
@@ -0,0 +1,5 @@
+# Load colors and export them as environment variables
+TEMPLATE="{{#.}}export COLOR{{@key}}=#{{hex.[0]}}{{hex.[1]}}{{hex.[2]}}
+{{/.}}"
+
+$(tstore template "color-themes.$(tstore template color-themes.selected {{.}})" "$TEMPLATE")
