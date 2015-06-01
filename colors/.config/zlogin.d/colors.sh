44e205eb6c6d5f4bc1ff914e8de01b48f0704926
Florian Albertz <flo.albertz@gmail.com>
2015-06-01 13:19:43 +0200

    Move color themes to their own files

--- colors/.config/zlogin.d/colors.sh
+++ colors/.config/zlogin.d/colors.sh
@@ -2,4 +2,4 @@
 TEMPLATE="{{#.}}export COLOR{{@key}}=#{{hex.[0]}}{{hex.[1]}}{{hex.[2]}}
 {{/.}}"
 
-$(tstore template "color-themes.$(tstore template color-themes.selected {{.}})" "$TEMPLATE")
+$(tstore template "color-themes/$(tstore template color-themes.selected {{.}})" "$TEMPLATE")
--- urxvt/.config/xinitrc.d/urxvt.sh
+++ urxvt/.config/xinitrc.d/urxvt.sh
@@ -5,4 +5,4 @@ xrdb -merge .Xresources
 TEMPLATE="{{#.}}URxvt*color{{@key}}: #{{hex.[0]}}{{hex.[1]}}{{hex.[2]}}
 {{/.}}"
 
-tstore template "color-themes.$(tstore template color-themes.selected {{.}})" "$TEMPLATE" | xrdb -merge
+tstore template "color-themes/$(tstore template color-themes.selected {{.}})" "$TEMPLATE" | xrdb -merge
