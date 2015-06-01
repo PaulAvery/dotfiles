150beccdd2f616324fe82847079f0e83be9bfc45
Florian Albertz <flo.albertz@gmail.com>
2015-06-01 13:19:14 +0200

    Read gnupghome instead of hardcoding this

--- gnupg/.config/zshrc.d/gnupg.sh
+++ gnupg/.config/zshrc.d/gnupg.sh
@@ -1,7 +1,7 @@
 export GPG_TTY="$(tty)"
 
 pgrep "gpg-agent" > /dev/null
-if [[ -a "/media/gpg/gnupg" && $? -ne 0 ]]; then
+if [[ -a $GNUPGHOME && $? -ne 0 ]]; then
 	echo "Starting gpg-agent"
 	gpg-agent --daemon > /dev/null 2> /dev/null || true
 fi
