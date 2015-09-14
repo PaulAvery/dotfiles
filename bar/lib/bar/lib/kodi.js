ac3e5a111919f71ccd9bbad13abcfbf33a37c1a1
Florian Albertz <flo.albertz@gmail.com>
2015-09-14 21:06:47 +0200

    Truncate long kodi titles

--- bar/lib/bar/lib/kodi.js
+++ bar/lib/bar/lib/kodi.js
@@ -1,6 +1,10 @@
 /* eslint-disable new-cap */
 var kodiws = require('xbmc-ws');
 
+function trunc(str, cut) {
+	return str.length > cut ? str.substr(0, cut - 3) + '...' : str;
+}
+
 module.exports = function(host) {
 	var kodi = kodiws(host, 9090);
 
@@ -32,7 +36,7 @@ module.exports = function(host) {
 				var symbol = status.speed === 0 ? ' | ' : ' > ';
 				var source = item.item.showtitle || item.item.artist;
 
-				return symbol + item.item.title + (source ? ' - ' + source : '') + ' ';
+				return symbol + trunc(item.item.title, 60) + trunc(source ? ' - ' + source : '', 20) + ' ';
 			} else {
 				return '';
 			}
