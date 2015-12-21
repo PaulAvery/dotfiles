127671c8c81c540a9c152f57cc0ae51ee4ed9b62
Florian Albertz <flo.albertz@gmail.com>
2015-12-21 12:48:49 +0100

    Change logic to mount and start gpg data

--- gnupg/.config/zshrc.d/gnupg.sh
+++ gnupg/.config/zshrc.d/gnupg.sh
@@ -1,7 +1,3 @@
 export GPG_TTY="$(tty)"
 
-pgrep "gpg-agent" > /dev/null
-if [[ -a $GNUPGHOME/gpg.conf && $? -ne 0 ]]; then
-	echo "Starting gpg-agent"
-	gpg-agent --daemon > /dev/null 2> /dev/null || true
-fi
+alias gpgm='sudo mount /dev/disk/by-partuuid/32a93336-f729-4ccd-8ecf-1c86515bcbd4 ~/gpg > /dev/null 2> /dev/null && pgrep gpg-agent > /dev/null && gpg-agent --daemon > /dev/null 2> /dev/null || true'
