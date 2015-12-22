5bda37506728d8886019d587e0e1ec3aea2096f6
Florian Albertz <flo.albertz@gmail.com>
2015-12-22 01:31:20 +0100

    Add gpg unmount alias

--- gnupg/.config/zshrc.d/gnupg.sh
+++ gnupg/.config/zshrc.d/gnupg.sh
@@ -1,3 +1,4 @@
 export GPG_TTY="$(tty)"
 
 alias gpgm='sudo mount /dev/disk/by-partuuid/32a93336-f729-4ccd-8ecf-1c86515bcbd4 ~/gpg > /dev/null 2> /dev/null && pgrep gpg-agent > /dev/null && gpg-agent --daemon > /dev/null 2> /dev/null || true'
+alias gpgu='sudo umount ~/gpg'
