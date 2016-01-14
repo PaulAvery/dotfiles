ad43920234ff26763cbe5e2f38f02a613229cba6
Florian Albertz <florian@albertz.io>
2016-01-14 22:17:23 +0100

    Try to keep bar from crashing on quitting kodi

--- bar/lib/bar/lib/kodi.js
+++ bar/lib/bar/lib/kodi.js
@@ -1,39 +1,55 @@
-/* eslint-disable new-cap */
+'use strict'
+
 var kodiws = require('kodi-ws');
 
 module.exports = function(host) {
-	var kodi = kodiws(host, 9090);
+	let kodi;
+	let clear = true;
+
+	function reconnect() {
+		if(clear) {
+			clear = false;
+			kodi = kodiws(host, 9090);
+			kodi.then((con) => {
+				con.on('error', reconnect);
+			});
+
+			setTimeout(() => clear = true, 500);
+		}
+	}
+
+	reconnect();
 
 	return {
 		get: function *() {
 			try {
-				var con = yield kodi;
+				let con = yield kodi;
+				let player = (yield con.Player.GetActivePlayers())[0];
+
+				if(player) {
+					var item = yield con.Player.GetItem(player.playerid, [
+						'title',
+						'artist',
+						'showtitle'
+					]);
+
+					var status = yield con.Player.GetProperties(player.playerid, [
+						'speed',
+						'type',
+						'time',
+						'percentage',
+						'totaltime'
+					]);
+
+					var symbol = status.speed === 0 ? ' | ' : ' > ';
+					var source = item.item.showtitle || item.item.artist;
+
+					return symbol + item.item.title + (source ? ' - ' + source : '') + ' ';
+				} else {
+					return '';
+				}
 			} catch(e) {
-				return '';
-			}
-
-			var player = (yield con.Player.GetActivePlayers())[0];
-
-			if(player) {
-				var item = yield con.Player.GetItem(player.playerid, [
-					'title',
-					'artist',
-					'showtitle'
-				]);
-
-				var status = yield con.Player.GetProperties(player.playerid, [
-					'speed',
-					'type',
-					'time',
-					'percentage',
-					'totaltime'
-				]);
-
-				var symbol = status.speed === 0 ? ' | ' : ' > ';
-				var source = item.item.showtitle || item.item.artist;
-
-				return symbol + item.item.title + (source ? ' - ' + source : '') + ' ';
-			} else {
+				reconnect();
 				return '';
 			}
 		},
