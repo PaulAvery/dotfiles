d4b52000188ba421c881fe64ea688bf1e8ef7d12
Florian Albertz <florian@albertz.io>
2016-01-21 12:24:56 +0100

    Remove gpg info from bar

--- bar/lib/bar/lib/gpg.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs');
-
-exports.get = function *() {
-	return (yield new Promise(function(res, rej) {
-		fs.stat('/home/florian/gpg/gpg.conf', e => res(!e));
-	})) ? '!' : '?';
-};
