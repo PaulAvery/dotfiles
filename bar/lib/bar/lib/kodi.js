5da5c9878f422ac88c5ce091b002075218fb2f36
Florian Albertz <florian@albertz.io>
2016-02-03 21:29:36 +0100

    New try fixing kodi for the bar

--- bar/lib/bar/lib/kodi.js
+++ bar/lib/bar/lib/kodi.js
@@ -3,53 +3,49 @@
 var kodiws = require('kodi-ws');
 
 module.exports = function(host) {
-	let kodi;
-	let clear = true;
+	let con;
+	let kodi = kodiws(host, 9090);
 
-	function reconnect() {
-		if(clear) {
-			clear = false;
-			kodi = kodiws(host, 9090);
-			kodi.then((con) => {
-				con.on('error', reconnect);
-			});
+	kodi.then(kodi => {
+		con = kodi;
 
-			setTimeout(() => clear = true, 500);
-		}
-	}
-
-	reconnect();
+		kodi.on('error', function() {
+			con = null;
+		});
+	});
 
 	return {
 		get: function *() {
-			try {
-				let con = yield kodi;
-				let player = (yield con.Player.GetActivePlayers())[0];
+			if(con) {
+				try {
+					let player = (yield con.Player.GetActivePlayers())[0];
 
-				if(player) {
-					var item = yield con.Player.GetItem(player.playerid, [
-						'title',
-						'artist',
-						'showtitle'
-					]);
+					if(player) {
+						var item = yield con.Player.GetItem(player.playerid, [
+							'title',
+							'artist',
+							'showtitle'
+						]);
 
-					var status = yield con.Player.GetProperties(player.playerid, [
-						'speed',
-						'type',
-						'time',
-						'percentage',
-						'totaltime'
-					]);
+						var status = yield con.Player.GetProperties(player.playerid, [
+							'speed',
+							'type',
+							'time',
+							'percentage',
+							'totaltime'
+						]);
 
-					var symbol = status.speed === 0 ? ' | ' : ' > ';
-					var source = item.item.showtitle || item.item.artist;
+						var symbol = status.speed === 0 ? ' | ' : ' > ';
+						var source = item.item.showtitle || item.item.artist;
 
-					return symbol + item.item.title + (source ? ' - ' + source : '') + ' ';
-				} else {
+						return symbol + item.item.title + (source ? ' - ' + source : '') + ' ';
+					} else {
+						return '';
+					}
+				} catch(e) {
 					return '';
 				}
-			} catch(e) {
-				reconnect();
+			} else {
 				return '';
 			}
 		},
