021c6d8e3bfb271f0966ff1d82bd5ba423a7d823
Florian Albertz <florian@albertz.io>
2016-02-24 19:35:44 +0100

    Fix small thing in quotes

--- quotes/.config/fish/functions/fish_greeting.fish
+++ quotes/.config/fish/functions/fish_greeting.fish
@@ -1,6 +1,6 @@
 function fish_greeting
-	if status --is-interactive then
-		if status --is-login then
+	if status --is-interactive
+		if status --is-login
 			if test -z "$DISPLAY"; and not test "$XDG_VTNR" = 1
 				# If we are a login shell and do not have a display AND are not on tty1 quote Firefly
 				shtore get quotes/firefly | quoty
